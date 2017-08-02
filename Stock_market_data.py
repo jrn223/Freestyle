31f31d5a602b36683b53d6e2214c572cf59fbed4
jrn223 <jrn223@stern.nyu.edu>
2017-08-01 22:52:08 -0400

    Difference between today and yesterday closing place

--- Stock_market_data.py
+++ Stock_market_data.py
@@ -18,27 +18,38 @@ daily_closing_prices = response.ix["Close"]
 
 print(daily_closing_prices)
 
+aapl_yesterday = daily_closing_prices.iloc[0]['AAPL']
+aapl_today = daily_closing_prices.iloc[1]['AAPL']
+print(aapl_today - aapl_yesterday)
+
+amzn_yesterday = daily_closing_prices.iloc[0]['AMZN']
+amzn_today = daily_closing_prices.iloc[1]['AMZN']
+print(amzn_today - amzn_yesterday)
+
+
+
+
 # AUTHENTICATE
 
-SENDGRID_API_KEY = os.environ.get('SENDGRID_API_KEY')
-
-sg = sendgrid.SendGridAPIClient(apikey = SENDGRID_API_KEY)
-
-# COMPILE REQUEST PARAMETERS
-
-subject = "Hello World from the SendGrid Python Library!"
-my_email = Email("jrn223@stern.nyu.edu")
-from_email = my_email
-to_email = my_email
-content = Content("text/plain", "Hello, Email!")
-mail = Mail(from_email, subject, to_email, content)
-
-# ISSUE REQUEST
-
-response = sg.client.mail.send.post(request_body=mail.get())
-
-# PARSE RESPONSE
-
-print(response.status_code)
-print(response.body)
-print(response.headers)
+# SENDGRID_API_KEY = os.environ.get('SENDGRID_API_KEY')
+#
+# sg = sendgrid.SendGridAPIClient(apikey = SENDGRID_API_KEY)
+#
+# # COMPILE REQUEST PARAMETERS
+#
+# subject = "Hello World from the SendGrid Python Library!"
+# my_email = Email("jrn223@stern.nyu.edu")
+# from_email = my_email
+# to_email = my_email
+# content = Content("text/plain", "Hello, Email!")
+# mail = Mail(from_email, subject, to_email, content)
+#
+# # ISSUE REQUEST
+#
+# response = sg.client.mail.send.post(request_body=mail.get())
+#
+# # PARSE RESPONSE
+#
+# print(response.status_code)
+# print(response.body)
+# print(response.headers)
